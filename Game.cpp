c26ead73163b0f8d6acbc87651f8edb5f6eb88bb
Shawn Lewis <shawnlewis@shawns-mbp.attlocal.net>
2019-08-30 13:07:35 -0700

    Partially created player

--- Game.cpp
+++ Game.cpp
@@ -2,7 +2,42 @@
 #include <string>
 using namespace std;
 
+class Player
+{
+    public:
+        void incMoney(float amt);
+        void decMoney(float amt);
+        float budget();
+    
+    private:
+        float money = 300;
+
+};
+
+void Player::incMoney(float amt)
+{
+    money = money + amt;
+    return;
+}
+
+void Player::decMoney(float amt)
+{
+    money = money - amt;
+    return;
+}
+
+float Player::budget()
+{
+    return money;
+}
+
 int main()
 {
+    Player Phil;
+    cout << Phil.budget() << endl;
+    Phil.decMoney(10);
+    cout << Phil.budget() << endl;
+    Phil.incMoney(50);
+    cout << Phil.budget() << endl;
     return 0;
 }
