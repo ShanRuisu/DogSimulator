538c930626f92b84e16957f0b5ed61f4f1f8c819
nyamamoto19 <nicholas.yamamoto@gmail.com>
2019-08-30 14:46:13 -0700

    coyote function

--- Game.cpp
+++ Game.cpp
@@ -28,6 +28,7 @@ class Dog
         void incHealth(int amt);
         void decHealth(int amt);
         void kill();
+        void coyote();
 
         // Accessor Functions
         string name();
@@ -87,6 +88,16 @@ void Dog::decHealth(int amt)
     return;
 }
 
+void Dog::coyote()
+{   
+    int fate;
+    cout << "You encounter an coyote. Oof!" << endl;
+    fate = rand() %9;
+
+
+
+
+}
 string Dog::name()
 {
     return m_name;
